5891d2f8fb3861ac99c04779161b397517b2875f
Tomislav Hengl <tom.hengl@opengeohub.org>
2021-12-16 16:15:50 +0100

    fixed bug with bind all

--- test_bind.R
+++ test_bind.R
@@ -1,10 +1,13 @@
 ## Bind all point data
+library(rgdal)
+library(plyr)
+library(terra)
 source("R-code/functions/SSL_functions.R")
 ossl.def = list(site.name, soilab.name, mir.name, visnir.name)
 names(ossl.def) = c("site.name", "soilab.name", "mir.name", "visnir.name")
 save(ossl.def, file="~/Documents/git/ossl/data/ossl.def.rda", compress="xz")
 
-## Soil wet-chemistry
+## Soil wet-chemistry ----
 soil.lst = list.files("/mnt/soilspec4gg/ossl/dataset", "ossl_soillab_v1.rds", full.names = TRUE, recursive = TRUE)
 soil.df = plyr::rbind.fill(lapply(soil.lst, readRDS.gz))
 ## duplicate columns
@@ -37,12 +40,12 @@ for(j in c("ec.w_usda.4f1_dsm")){
   soil.df[,j] = ifelse(soil.df[,j]>50|soil.df[,j]<0, NA, soil.df[,j])
 }
 dim(soil.df)
-# 119730     60
+# 120550     59
 summary(is.na(soil.df$id.layer_uuid_c))
 ## OK!
 soil.df = dplyr::distinct(soil.df)
 
-## Sampling locations
+## Sampling locations ----
 site.lst = list.files("/mnt/soilspec4gg/ossl/dataset", "ossl_soilsite_v1.rds", full.names = TRUE, recursive = TRUE)
 site.df = plyr::rbind.fill(lapply(site.lst, readRDS.gz))
 ## remove total duplicates
@@ -50,7 +53,7 @@ site.df$observation.date.begin_iso.8601_yyyy.mm.dd = paste(site.df$observation.d
 site.df$observation.date.end_iso.8601_yyyy.mm.dd = paste(site.df$observation.date.end_iso.8601_yyyy.mm.dd)
 site.df = dplyr::distinct(site.df)
 dim(site.df)
-# 123889     38
+# 124709     38
 #del2.col = names(site.df)[grep(glob2rx("*.1$"), names(site.df))]
 #summary(site.df$location.error_any_m)
 ## If location accuracy is missing, we assume that location accuracy is GPS
@@ -59,11 +62,11 @@ site.df$location.error_any_m = ifelse(is.na(site.df$location.error_any_m) & !is.
 #hist(site.df$location.error_any_m, breaks=35)
 site.df$location.method_any_c = ifelse(is.na(site.df$location.method_any_c) & !is.na(site.df$longitude_wgs84_dd), "GPS", site.df$location.method_any_c)
 summary(as.factor(site.df$dataset.code_ascii_c))
-# AFSIS1.SSL     CAF.SSL ICRAF.ISRIC    KSSL.SSL   LUCAS.SSL    NEON.SSL
-#       4536        1852        4239      117218       43927         304
+#AFSIS1.SSL  AFSIS2.SSL     CAF.SSL ICRAF.ISRIC    KSSL.SSL   LUCAS.SSL    NEON.SSL
+#      4536         820        1852        4239       69031       43927         304
 summary(is.na(site.df$id.layer_uuid_c))
 ## some are missing ID
-## 4852
+## 4808
 site.df$id.layer_local_c = plyr::join(site.df["id.layer_uuid_c"], soil.df[,c("id.layer_local_c","id.layer_uuid_c")], match="first")$id.layer_local_c
 ## sampling locations
 site.xy = site.df[!duplicated(site.df$id.location_olc_c),]
@@ -75,7 +78,7 @@ proj4string(site.xy) = "EPSG:4326"
 unlink("out/ossl_locations.gpkg")
 writeOGR(site.xy[c("id.location_olc_c","dataset.code_ascii_c")], "out/ossl_locations.gpkg", "", "GPKG")
 
-## Bind all VisNIR
+## Bind all VisNIR ----
 visnir.lst = list.files("/mnt/soilspec4gg/ossl/dataset", "ossl_visnir_v1.rds", full.names = TRUE, recursive = TRUE)
 visnir.df = plyr::rbind.fill(lapply(visnir.lst, readRDS.gz))
 dim(visnir.df)
@@ -90,7 +93,7 @@ summary(as.factor(visnir.df$model.code_any_c))
 visnir.pnts = site.df[site.df$id.layer_uuid_c %in% visnir.df$id.layer_uuid_c, c("longitude_wgs84_dd", "latitude_wgs84_dd", "id.location_olc_c")]
 visnir.pnts = visnir.pnts[!is.na(visnir.pnts$longitude_wgs84_dd),]
 visnir.pnts = SpatialPointsDataFrame(visnir.pnts[,1:2], data = visnir.pnts["id.location_olc_c"], proj4string = CRS("EPSG:4326"))
-## World map
+## World map ----
 if(!file.exists("img/visnir.pnts_sites.png")){
   library(sf)
   tot_pnts_sf <- st_as_sf(visnir.pnts[1])
@@ -99,7 +102,7 @@ if(!file.exists("img/visnir.pnts_sites.png")){
   system("convert -crop 1280x575+36+114 img/visnir.pnts_sites.png img/visnir.pnts_sites.png")
 }
 
-## Bind all MIR
+## Bind all MIR ----
 mir.lst = list.files("/mnt/soilspec4gg/ossl/dataset", "ossl_mir_v1.rds", full.names = TRUE, recursive = TRUE)
 ## 6
 mir.df = plyr::rbind.fill(lapply(mir.lst, readRDS.gz))
@@ -124,19 +127,23 @@ if(!file.exists("img/mir.pnts_sites.png")){
   system("convert -crop 1280x575+36+114 img/mir.pnts_sites.png img/mir.pnts_sites.png")
 }
 
-## Save final images
+## Save final images ----
 ## Rule 1: save only visnir/mir/soil data that match i.e. no orphaned records
 id.vnir = intersect(soil.df$id.layer_uuid_c, visnir.df$id.layer_uuid_c)
 id.mir = intersect(soil.df$id.layer_uuid_c, mir.df$id.layer_uuid_c)
 id.s = intersect(soil.df$id.layer_uuid_c, site.df$id.layer_uuid_c)
+length(id.s)
+# 119884
+dim(mir.df[mir.df$id.layer_uuid_c %in% id.mir,])
+# 74572  1718
 dim(visnir.df[visnir.df$id.layer_uuid_c %in% id.vnir,])
 # 100586   1092
 ## Rule 2: fill in important mising fields such as location accuracy / attribution
 ## Rule 3: check that the data can be bind together to produce a regression matrix
 ## Rule 4: fix all wrong emails etc
-#soil.df$sample.contact.email_ietf_email = ifelse(soil.df$sample.contact.email_ietf_email=="esdac@jrc.ec.europa.eu", "ec-esdac@jrc.ec.europa.eu", soil.df$sample.contact.email_ietf_email)
-#site.df$surveyor.contact_ietf_email = ifelse(site.df$surveyor.contact_ietf_email=="esdac@jrc.ec.europa.eu", "ec-esdac@jrc.ec.europa.eu", site.df$surveyor.contact_ietf_email)
-#site.df$dataset.contact_ietf_email = ifelse(site.df$dataset.contact_ietf_email=="esdac@jrc.ec.europa.eu", "ec-esdac@jrc.ec.europa.eu", site.df$dataset.contact_ietf_email)
+soil.df$sample.contact.email_ietf_email = ifelse(soil.df$sample.contact.email_ietf_email=="esdac@jrc.ec.europa.eu", "ec-esdac@jrc.ec.europa.eu", soil.df$sample.contact.email_ietf_email)
+site.df$surveyor.contact_ietf_email = ifelse(site.df$surveyor.contact_ietf_email=="esdac@jrc.ec.europa.eu", "ec-esdac@jrc.ec.europa.eu", site.df$surveyor.contact_ietf_email)
+site.df$dataset.contact_ietf_email = ifelse(site.df$dataset.contact_ietf_email=="esdac@jrc.ec.europa.eu", "ec-esdac@jrc.ec.europa.eu", site.df$dataset.contact_ietf_email)
 summary(as.factor(site.df$dataset.contact_ietf_email))
 #visnir.df$scan.contact.email_ietf_email = ifelse(visnir.df$scan.contact.email_ietf_email=="esdac@jrc.ec.europa.eu", "ec-esdac@jrc.ec.europa.eu", visnir.df$scan.contact.email_ietf_email)
 summary(as.factor(mir.df$scan.contact.email_ietf_email))
@@ -147,14 +154,16 @@ saveRDS.gz(visnir.df[visnir.df$id.layer_uuid_c %in% id.vnir,], "/mnt/soilspec4gg
 saveRDS.gz(mir.df[mir.df$id.layer_uuid_c %in% id.mir,], "/mnt/soilspec4gg/ossl/ossl_import/ossl_mir_v1.rds")
 #rm(mir.df); gc()
 #rm(visnir.df); gc()
+#visnir.df = readRDS.gz("/mnt/soilspec4gg/ossl/ossl_import/ossl_visnir_v1.rds")
+#mir.df = readRDS.gz("/mnt/soilspec4gg/ossl/ossl_import/ossl_mir_v1.rds")
 
-## Overlay points and covariates
+## Overlay points and covariates ----
 ov_ADMIN = readOGR("/mnt/gaia/tmp/openlandmap/tiling/tiles_GH_100km_land.gpkg")
 site.p = spTransform(site.xy, ov_ADMIN@proj4string)
 ID = sp::over(site.p, ov_ADMIN)
-summary(as.factor(ID$ID))
+#summary(as.factor(ID$ID))
 tif.lst = list.files("/data/WORLDCLIM", ".tif", full.names=TRUE)
-## 62
+## 86
 ov.tmp = parallel::mclapply(1:length(tif.lst), function(j){ terra::extract(terra::rast(tif.lst[j]), terra::vect(site.xy)) }, mc.cores = 32)
 ov.tmp = dplyr::bind_cols(lapply(ov.tmp, function(i){i[,2]}))
 names(ov.tmp) = tools::file_path_sans_ext(basename(tif.lst))
@@ -163,12 +172,13 @@ c.na = sapply(ov.tmp, function(i){sum(is.na(i))})
 c.sd = sapply(ov.tmp, function(i){var(i, na.rm=TRUE)})
 rm.c = which(c.na>200 | c.sd == 0)
 #rm.c
+## clm_snow.prob missing values for 300-400 points
 ov.tmp$id.location_olc_c = site.xy$id.location_olc_c
 ov.tmp$ID = ID$ID
 #summary(as.factor(ov.tmp$ID))
 ## high clustering of points?
 
-## Produce final regression matrix:
+## Final regression matrix ----
 gc()
 #soilsite = readRDS(url("http://s3.us-east-1.wasabisys.com/soilspectroscopy/ossl_import/ossl_soilsite_v1.rds", "rb"))
 #soillab = readRDS(url("http://s3.us-east-1.wasabisys.com/soilspectroscopy/ossl_import/ossl_soillab_v1.rds", "rb"))
@@ -178,16 +188,19 @@ gc()
 ## create 1 regression matrix with all data
 visnir.df$vnirmodel.code_any_c = paste(visnir.df$model.code_any_c)
 mir.df$mirmodel.code_any_c = paste(mir.df$model.code_any_c)
-sel.mir.name = c("id.scan_uuid_c", "id.layer_uuid_c", "mirmodel.code_any_c", paste0("scan_mir.", seq(600, 4000, by=2), "_abs"))
-sel.vnir.name = c("id.scan_uuid_c", "id.layer_uuid_c", "vnirmodel.code_any_c", paste0("scan_visnir.", seq(350, 2500, by=2), "_pcnt"))
+sel.mir.name = c("id.layer_uuid_c", "mirmodel.code_any_c", paste0("scan_mir.", seq(600, 4000, by=2), "_abs"))
+sel.vnir.name = c("id.layer_uuid_c", "vnirmodel.code_any_c", paste0("scan_visnir.", seq(350, 2500, by=2), "_pcnt"))
 ## takes minutes...
 rm.ossl = plyr::join_all(list(soil.df[soil.df$id.layer_uuid_c %in% id.s,],
                          site.df[site.df$id.layer_uuid_c %in% id.s,],
-                         visnir.df[visnir.df$id.layer_uuid_c %in% id.vnir,sel.vnir.name],
-                         mir.df[mir.df$id.layer_uuid_c %in% id.mir,sel.mir.name],
+                         visnir.df[visnir.df$id.layer_uuid_c %in% id.vnir, sel.vnir.name],
+                         mir.df[mir.df$id.layer_uuid_c %in% id.mir, sel.mir.name],
                          ov.tmp))
+#rm.ossl = rm.ossl[!is.na(rm.ossl$dataset.code_ascii_c),]
 dim(rm.ossl)
-## 150737   2939
+## 152146   2963
+summary(!is.na(rm.ossl$scan_mir.602_abs))
+summary(!is.na(rm.ossl$scan_visnir.452_pcnt))
 ## replace missing block ID's
 summary(is.na(rm.ossl$ID))
 rm.ossl$location.address_utf8_txt = ifelse(is.na(rm.ossl$location.address_utf8_txt), "Unknown", rm.ossl$location.address_utf8_txt)
@@ -195,20 +208,3 @@ rm.ossl$ID = ifelse(is.na(rm.ossl$ID), rm.ossl$location.address_utf8_txt, rm.oss
 saveRDS.gz(rm.ossl, "/mnt/soilspec4gg/ossl/ossl_import/rm.ossl_v1.rds")
 #ossl.x=rm.ossl[sample.int(nrow(rm.ossl), 3e4),]
 
-## test subset function:
-source("R-code/functions/SSL_functions.R")
-summary(rm.ossl$layer.upper.depth_usda_cm)
-## 830 NA + some very high values
-## MIR only
-ph.rm.mir.kssl = subset.ossl(rm.ossl, dataset.y="KSSL.SSL", geo.x=NULL, visnir.x=NULL)
-dim(ph.rm.mir.kssl)
-ph.rm.mir = subset.ossl(rm.ossl, geo.x=NULL, visnir.x=NULL)
-dim(ph.rm.mir)
-ph.rm.nir = subset.ossl(rm.ossl, geo.x=NULL, mir.x=NULL)
-dim(ph.rm.nir)
-ph.rm.mirnir = subset.ossl(rm.ossl, geo.x=NULL)
-dim(ph.rm.mirnir)
-ph.rm.all = subset.ossl(rm.ossl)
-dim(ph.rm.all)
-
-oc.rm = subset.ossl(ossl.x, tvar.y="oc_usda.calc_wpct")
