eec3005cc45c15c2b8c7b12b665602572c2913df
José Lucas Safanelli <zecojls@gmail.com>
2026-04-29 00:36:06 +0000

    new dataset coding and work env defined

--- R/s1_move_files_from_gdrive_to_gcloud.R
+++ R/s1_move_files_from_gdrive_to_gcloud.R
@@ -71,11 +71,11 @@ dir_ls("~/")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/CAF"), sep = "\n")
 
-#### PlantedForests_NewZealand ####
+#### PF_NZL ####
 
 dir_ls("~/")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
-dir_create("~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand")
+dir_create("~/mnt-ossl-private/database/datasets/PF_NZL")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
 
 print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
@@ -83,30 +83,30 @@ print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
 print(drive_ls(as_id("15UCzVjsLIcg_XKsW3yLIppwzQRGAeAAs"), n=Inf))
 
 # drive_download(file = as_id("1AI9Vbszdo8qRWUnUhwyEOrJtKTgeJ__I"),
-#                path = "~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/Garrett.zip",
+#                path = "~/mnt-ossl-private/database/datasets/PF_NZL/Garrett.zip",
 #                overwrite = T, verbose = T)
 #
-# unzip(zipfile = "~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/Garrett.zip",
+# unzip(zipfile = "~/mnt-ossl-private/database/datasets/PF_NZL/Garrett.zip",
 #       list=T)
 #
-# all_files <- unzip("~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/Garrett.zip", list = TRUE)$Name
+# all_files <- unzip("~/mnt-ossl-private/database/datasets/PF_NZL/Garrett.zip", list = TRUE)$Name
 # files_to_extract <- all_files[!grepl("\\.(0|jpg|JPG|tif|GIF)$", all_files, ignore.case = TRUE)]
 #
-# unzip("~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/Garrett.zip",
+# unzip("~/mnt-ossl-private/database/datasets/PF_NZL/Garrett.zip",
 #       files = files_to_extract,
-#       exdir = "~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/")
+#       exdir = "~/mnt-ossl-private/database/datasets/PF_NZL/")
 #
-# dir_ls("~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/")
+# dir_ls("~/mnt-ossl-private/database/datasets/PF_NZL/")
 
-# src <- "/home/jsafanelli/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/Garrett"
-# dest <- "/home/jsafanelli/mnt-ossl-private/database/datasets/PlantedForests_NewZealand"
+# src <- "/home/jsafanelli/mnt-ossl-private/database/datasets/PF_NZL/Garrett"
+# dest <- "/home/jsafanelli/mnt-ossl-private/database/datasets/PF_NZL"
 # items <- list.files(src, full.names = TRUE, recursive = T)
 # new_items <- gsub("/Garrett", "", items)
-# dir_create("~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/FR380_MIR spectra_csv")
+# dir_create("~/mnt-ossl-private/database/datasets/PF_NZL/FR380_MIR spectra_csv")
 # for(i in 1:length(items)){file_move(items[i], new_items[i])}
-# dir_delete("~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/Garrett")
+# dir_delete("~/mnt-ossl-private/database/datasets/PF_NZL/Garrett")
 
-dir_ls("~/mnt-ossl-private/database/datasets/PlantedForests_NewZealand/")
+dir_ls("~/mnt-ossl-private/database/datasets/PF_NZL/")
 
 #### ICRAF_ISRIC ####
 
@@ -139,7 +139,7 @@ print(drive_ls(as_id("1E2mEQqMqqCecG_iq1CXq2kXXYLi-Raf-"), n=Inf))
 # for(i in 1:length(items)){file_move(items[i], new_items[i])}
 # dir_delete("~/mnt-ossl-private/database/datasets/ICRAF_ISRIC/ICRAF_ISRIC")
 
-#### ICRAF_ISRIC ####
+#### KSSL ####
 
 dir_ls("~/")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
@@ -236,11 +236,11 @@ print(drive_ls(as_id("14CPNgelVHNc67iWx_-95qU8HBFNBWSS1")), n=Inf)
 # file_move("~/mnt-ossl-private/database/datasets/Neospectra.zip",
 #           "~/mnt-ossl-private/database/datasets/Neospectra/Neospectra.zip")
 
-#### HighLatitudeForests_NorthCanada ####
+#### HLF_CAN ####
 
 dir_ls("~/")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
-dir_create("~/mnt-ossl-private/database/datasets/HighLatitudeForests_NorthCanada/")
+dir_create("~/mnt-ossl-private/database/datasets/HLF_CAN/")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
 
 print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
@@ -261,18 +261,18 @@ print(drive_ls(as_id("1HfKDkhS7XCqc2GjxUM0guhzKxyoteBy9")), n=Inf)
 #       exdir = "~/mnt-ossl-private/database/datasets/")
 #
 # file_move("~/mnt-ossl-private/database/datasets/Schiedung.zip",
-#           "~/mnt-ossl-private/database/datasets/HighLatitudeForests_NorthCanada/Schiedung.zip")
+#           "~/mnt-ossl-private/database/datasets/HLF_CAN/Schiedung.zip")
 #
 # items <- dir_ls("~/mnt-ossl-private/database/datasets/Schiedung/", full.names = TRUE)
-# new_items <- gsub("/Schiedung/", "/HighLatitudeForests_NorthCanada/", items)
+# new_items <- gsub("/Schiedung/", "/HLF_CAN/", items)
 # for(i in 1:length(items)){file_move(items[i], new_items[i])}
 # dir_delete("~/mnt-ossl-private/database/datasets/Schiedung")
 
-#### Serbian ####
+#### SRB ####
 
 dir_ls("~/")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
-dir_create("~/mnt-ossl-private/database/datasets/Serbian/")
+dir_create("~/mnt-ossl-private/database/datasets/SRB/")
 cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
 
 print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
@@ -289,9 +289,164 @@ print(drive_ls(as_id("1_lDtb7grO5SbTIHw1c3VmxPhh9BEWVhf")), n=Inf)
 #       exdir = "~/mnt-ossl-private/database/datasets")
 #
 # file_move("~/mnt-ossl-private/database/datasets/Serbian.zip",
-#           "~/mnt-ossl-private/database/datasets/Serbian/Serbian.zip")
+#           "~/mnt-ossl-private/database/datasets/SRB/Serbian.zip")
 #
 # items <- dir_ls("~/mnt-ossl-private/database/datasets/Serbia/", full.names = TRUE)
-# new_items <- gsub("/Serbia/", "/Serbian/", items)
+# new_items <- gsub("/Serbia/", "/SRB/", items)
 # for(i in 1:length(items)){file_move(items[i], new_items[i])}
 # dir_delete("~/mnt-ossl-private/database/datasets/Serbia")
+
+# BESB
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/BESB")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1g2X3Veh1CS65pYaEV6CRhkNokrQmqU2N")), n=Inf)
+print(drive_ls(as_id("1ixBhKGV_C9fntf0mLL7WIU2Lxr95P7aT")), n=Inf)
+
+# drive_download(file = as_id("1Mf9z34XcSmGvgcBSpe1_epOAPwzSzveR"),
+#                path = "~/mnt-ossl-private/database/datasets/BESB/BSSL_DB_V002.xlsx",
+#                overwrite = T, verbose = T)
+
+# GEMAS
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/GEMAS")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1phRmQRB0yZN-aLHoMAFQaAk8znQBkze3")), n=Inf)
+print(drive_ls(as_id("1cxnJP7fA2JJVw1WTHHvCUX325XMOx_JU")), n=Inf)
+
+# drive_download(file = as_id("1a2uFNxmDlC5glq6KTiiHjTTl9oSTyEyS"),
+#                path = "~/mnt-ossl-private/database/datasets/GEMAS/GEMAS_MIRcompiled.csv",
+#                overwrite = T, verbose = T)
+
+# Geocradle
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/Geocradle")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1pMKqCOMB4zJlUofa3kSpYZv3Jv3VpPjV")), n=Inf)
+print(drive_ls(as_id("1U97agV7uXD_2eqxTrEjgQJ6jt6LsBIm-")), n=Inf)
+
+# drive_download(file = as_id("1TkDvffXivsJJuARyR7ON4QfVeytU_b3p"),
+#                path = "~/mnt-ossl-private/database/datasets/Geocradle/SSL_GEOCRADLE_1.csv",
+#                overwrite = T, verbose = T)
+
+# HSDOS
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/HSDOS")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1dYFR0LJO43lhWbcmavN3T53MJN-KbeKd")), n=Inf)
+print(drive_ls(as_id("1oN_JsujwqML21OchgNLmvmDa0HPE_ULr")), n=Inf)
+
+# drive_download(file = as_id("1spxchKUgS3uy1nDDy93mjMOA81oY4DBa"),
+#                path = "~/mnt-ossl-private/database/datasets/HSDOS/HSDOS_SSL_ver1.1.csv",
+#                overwrite = T, verbose = T)
+
+# GPKOR
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/GPKOR")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1Vf4Bjb8nI45tzGbby9pggPwN5hBvVx8b")), n=Inf)
+print(drive_ls(as_id("1j25_19dVsWUx27zjaepw_dNftMqOr08X")), n=Inf)
+
+# drive_download(file = as_id("1sXh5RNqoJEpoCcVxjWXdImoXEdLJkm_-"),
+#                path = "~/mnt-ossl-private/database/datasets/GPKOR/Gyeonggi Soil Spectral Library (G-SSL).xlsx",
+#                overwrite = T, verbose = T)
+
+# AUT
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/AUT")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1CsVQOVYae7GxfH1qH21IBFJJ3NSdlSF6")), n=Inf)
+print(drive_ls(as_id("1sM8eSLg3FDfqZKEu8-DfOGykZfHAlYAT")), n=Inf)
+
+# drive_download(file = as_id("1mIAYD7qEmfElwsANjnhS4yfr4ZGKJRbs"),
+#                path = "~/mnt-ossl-private/database/datasets/AUT/Austrian NIR Soil Spectral Library_V4.xlsx",
+#                overwrite = T, verbose = T)
+
+# LF_BRA
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/LF_BRA")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1kzib6MFLK5_-ssOlNJo_yu2MFSy4U_AT")), n=Inf)
+print(drive_ls(as_id("1ErtuFE8wAYNkYIp7N6937wDyBY5McyYP")), n=Inf)
+
+# drive_download(file = as_id("1Q_BhIHFjAnylHdzaRYY8uStDU-hkxGEs"),
+#                path = "~/mnt-ossl-private/database/datasets/LF_BRA/VNIR data.xlsx",
+#                overwrite = T, verbose = T)
+#
+# drive_download(file = as_id("16NdbJgjOR5h17vHt33dxREjXCe8YB1pN"),
+#                path = "~/mnt-ossl-private/database/datasets/LF_BRA/soil fertility data.xlsx",
+#                overwrite = T, verbose = T)
+
+# MTQ
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/MTQ")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1V_mdX_js_LxcqVN4_s6y8ijJpR-CUGlO")), n=Inf)
+print(drive_ls(as_id("1z0fixd85lTUozWTb1rGTOxLrqpDU_unj")), n=Inf)
+print(drive_ls(as_id("1PdMGn256HncUntwYuphZszhC-m6rGE4B")), n=Inf)
+
+# drive_download(file = as_id("1-BIp9yCYnOn6GL7HduRa2ZKx93wkhj28"),
+#                path = "~/mnt-ossl-private/database/datasets/MTQ/SoilCarbonMartinique_v2.tab",
+#                overwrite = T, verbose = T)
+
+# WS_SWIND
+
+dir_ls("~/")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+dir_create("~/mnt-ossl-private/database/datasets/WS_SWIND")
+cat(dir_ls("~/mnt-ossl-private/database/datasets/"), sep = "\n")
+
+print(drive_get(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1l9VM4fFks2xBloDE69EFTfPgQeFx5aGw")), n=Inf)
+print(drive_ls(as_id("1QAd6-MpG-0wWQ5Az5cOpmZag1HCObgGb")), n=Inf)
+print(drive_ls(as_id("1Qil9ZQwxboxFiounUsZkhCEFJ64XO-s6")), n=Inf)
+# view(drive_ls(as_id("1PG7kNmv4S0t3pPKEBTdMQOsvqDi2Bd8P")))
+# view(drive_ls(as_id("1ypJUs-tPfNNfvZ8oqWGP4lLt7i5CNckV")))
+
+# drive_download(file = as_id("1tJfy3KMbM7YCY0J7vrFMKikKD2bumrEO"),
+#                path = "~/mnt-ossl-private/database/datasets/WS_SWIND/20211111_MS_SOC_Stock_India_data_composites.csv",
+#                overwrite = T, verbose = T)
+#
+# drive_download(file = as_id("1ZIj5OsFd0vVA_qmY_LxFCbFBfaAJ8xWE"),
+#                path = "~/mnt-ossl-private/database/datasets/WS_SWIND/20211111_MS_SOC_Stock_India_data_raw.csv",
+#                overwrite = T, verbose = T)
